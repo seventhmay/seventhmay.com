dfd1f418f91f19898c8189df7f2e5dbe37453d65
Shinichi Ozutsumi <fabuleux.5.7th@gmail.com>
2013-03-01 22:04:18 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeventhmayCom::Application.config.secret_token = '0fc3d1db4f2e539fd097d586e06419996324aa65d55bd87d9cc1972d0541fdabd2929512da505b8be388cad2a043527d62c8d3cd917cb6cb06b6d16332cbff77'
