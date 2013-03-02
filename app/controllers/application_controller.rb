9eaa7572191fc8df965d74c7ff2a763c92b7267c
Shinichi Ozutsumi <fabuleux.5.7th@gmail.com>
2013-03-02 11:53:09 +0900

    added basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate, :except => [:index, :show]
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |u, p|
+      u == Settings.admin.name && p == Settings.admin.password
+    end
+  end
 end
--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,4 +1,6 @@
 class AppsController < ApplicationController
+  before_filter :authenticate
+
   # GET /apps
   # GET /apps.json
   def index
