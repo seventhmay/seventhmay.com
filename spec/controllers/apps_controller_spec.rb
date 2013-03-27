f0b0928bab64d0a69f6a8daf15b4b42705dc25fd
Shinichi Ozutsumi <fabuleux.5.7th@gmail.com>
2013-03-28 00:06:31 +0900

    add RSpec

--- spec/controllers/apps_controller_spec.rb
+++ spec/controllers/apps_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe AppsController do
+  fixtures :apps
+  describe 'responding to GET index' do
+    it 'should response.should be_success' do
+      get :index 
+      response.should be_success
+    end
+  end
 
+  describe 'responding to GET show' do
+    it 'should response.should be_success' do
+      get :show, :id => 1
+      response.should be_success
+    end
+  end
+
+  describe 'responding to GET new' do
+    it "should return 401 Not Authenticated" do
+      get :new
+      response.response_code.should == 401
+    end
+  end
 end
